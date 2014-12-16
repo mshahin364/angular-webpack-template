58334f13549ef25ee2580b9fda7b30ba91a3ecf3
Jean RAKOTOZAFY <jean.r@m-ite.com>
2014-12-16 11:40:05 +0300

    Remove duplicate controller file

--- controllers/mainCtrl.js
+++
@@ -1,7 +0,0 @@
-var angular = require('../lib/angular');
-
-angular.module('myApp')
-  .controller('mainCtrl', function($scope) {
-    $scope.firstName = 'John';
-    $scope.lastName = 'Doe';
-  });
